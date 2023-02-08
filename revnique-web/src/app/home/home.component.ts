1b8a43034c56503a25d997dff5dd1876ea431906
revnique <revnique@gmail.com>
2023-02-07 20:05:02 -0600

    fixed layout

--- revnique-web/src/app/home/home.component.ts
+++ revnique-web/src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RevProject, RevService } from '../services/rev.service';
+import { map } from 'rxjs/operators';
+import { EMPTY, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+
+  projects$: Observable<any> = EMPTY;
+  constructor(private svc: RevService) { }
+
+  ngOnInit() {
+    this.projects$ = this.svc.getProjects();
+  }
 }
--- revnique-web/src/app/services/rev.service.ts
+++ revnique-web/src/app/services/rev.service.ts
@@ -10,25 +10,28 @@ export class RevService {
     svcRoot: string = location.href.indexOf('localhost:') > -1 ? 'http://localhost:57777/api/' : 'https://api.bucktrace.com/';
     constructor(private http: HttpClient) { }
 
-    getProjects(SN: string): Observable<RevProject[]> {
+    getProjects(): Observable<RevProject[]> {
         let p: RevProject[] = [
             {
-                projectName: 'h1teq.com',
-                projectComment: '',
-                projectImage: '',
-                projectUrl: 'https://h1teq.com'
+                name: 'h1teq.com',
+                comment: 'h1 teq llc homepage',
+                image: '../../assets/images/rev_h1teq.png',
+                projectUrl: 'https://h1teq.com',
+                techList: ['responsive web', 'dynamic pdf creation', 'HTML', 'CSS']
             },
             {
-                projectName: 'bucktrace.com',
-                projectComment: '',
-                projectImage: '',
-                projectUrl: 'https://bucktrace.com'
+                name: 'bucktrace.com',
+                comment: 'a currency tracking application',
+                image: '../../assets/images/rev_bucktrace.png',
+                projectUrl: 'https://bucktrace.com',
+                techList: ['flutter', 'iOS', 'Android', 'node.js', 'AWS Lambda', 'mongoDb', 'Angular 14', 'React 18', 'SASS', 'AWS']
             },
             {
-                projectName: 'meridian',
-                projectComment: '',
-                projectImage: '',
-                projectUrl: 'https://meridian.ui.h1teq.com/proof/detail/5a051eaa0dd7185b4c2e7a2f'
+                name: 'meridian',
+                comment: 'peer to peer proof review system',
+                image: '../../assets/images/rev_meridian.png',
+                projectUrl: 'https://meridian.ui.h1teq.com/proof/detail/5a051eaa0dd7185b4c2e7a2f',
+                techList: ['Angular 4', 'web sockets', '.Net Core', 'C#']
             },
         ];
         return of(p);
@@ -37,9 +40,10 @@ export class RevService {
 
 
 export class RevProject {
-    public projectName: string = '';
-    public projectImage: string = '';
-    public projectComment: string = '';
+    public name: string = '';
+    public image: string = '';
+    public comment: string = '';
     public projectUrl: string = '';
+    public techList: string[] = [];
 }
 

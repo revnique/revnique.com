6d5eec8a760cd638512a62a45640226824470150
Dominique L. Harris <dharris@harristeq.local>
2023-02-11 22:18:22 -0600

    added api

--- revnique-web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ revnique-web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "restApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "function": {
     "getProjects": {
       "Arn": "string",

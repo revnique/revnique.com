1120160dad8da6f3fc9819a1708c6be2e4b86b4e
Dominique Harris <revnique@gmail.com>
2025-03-08 19:09:53 -0600

    to ng 18

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { RevService } from "../services/rev.service";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { HomeComponent } from "./home.component";
 import { ProjectComponent } from './components/project/project.component';
 import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         HomeComponent,
         ProjectComponent,
         ProjectDetailComponent
     ],
-    exports: [],
-    imports: [
-        SharedModule,
-        HttpClientModule
-    ],
-    providers: [RevService]
-})
+    exports: [], imports: [SharedModule], providers: [RevService, provideHttpClient(withInterceptorsFromDi())] })
 export class HomeModule { }
